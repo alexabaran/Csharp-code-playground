5d7a2c22e2a2c6e07cff2fa3e98fcad937965e75
Aleksandra Baran <aleksa.mierzwa@gmail.com>
2025-01-27 20:17:39 +0100

    changes in project 3 code

--- Algoruthms-and-Data-Structures-University/AISD_Projekt3/Program.cs
+++ Algoruthms-and-Data-Structures-University/AISD_Projekt3/Program.cs
@@ -14,7 +14,7 @@ class Program
         // Ustawienie licencji "NonCommercial"
         ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
 
-        // Definicja grafów - macierze sasiedztwa
+        // Definicja grafów - macierze sąsiedztwa
         int[,] graphA = {
             { 0, 1, 0, 1, 0, 1, 0, 0 },
             { 1, 0, 1, 1, 1, 0, 0, 0 },
@@ -69,25 +69,31 @@ class Program
         worksheet.Cells[1, 1].Value = "Graf";
         worksheet.Cells[1, 2].Value = "Algorytm";
         worksheet.Cells[1, 3].Value = "Średni czas [ns]";
+        worksheet.Cells[1, 4].Value = "Kroki";
+        worksheet.Cells[1, 5].Value = "Kolejność odwiedzania";
 
         int row = 2;
 
         foreach (var (graph, graphName) in graphs)
         {
             // Testowanie DFS
-            var dfsTimes = MeasureAlgorithm(graph, DFS);
+            var (dfsTimes, dfsSteps, dfsOrder) = MeasureAlgorithm(graph, DFS);
             long avgDfsTime = CalculateAverageExcludingMinMax(dfsTimes);
             worksheet.Cells[row, 1].Value = graphName;
             worksheet.Cells[row, 2].Value = "DFS";
             worksheet.Cells[row, 3].Value = avgDfsTime;
+            worksheet.Cells[row, 4].Value = dfsSteps;
+            worksheet.Cells[row, 5].Value = string.Join(", ", dfsOrder);
             row++;
 
             // Testowanie BFS
-            var bfsTimes = MeasureAlgorithm(graph, BFS);
+            var (bfsTimes, bfsSteps, bfsOrder) = MeasureAlgorithm(graph, BFS);
             long avgBfsTime = CalculateAverageExcludingMinMax(bfsTimes);
             worksheet.Cells[row, 1].Value = graphName;
             worksheet.Cells[row, 2].Value = "BFS";
             worksheet.Cells[row, 3].Value = avgBfsTime;
+            worksheet.Cells[row, 4].Value = bfsSteps;
+            worksheet.Cells[row, 5].Value = string.Join(", ", bfsOrder);
             row++;
         }
 
@@ -97,33 +103,42 @@ class Program
         Console.WriteLine("Wyniki zapisane do pliku Excel.");
     }
 
-    // Pomiar czasu dla algorytmu
-    public static long[] MeasureAlgorithm(int[,] graph, Action<int[,], int> algorithm)
+    public static (long[], int, List<int>) MeasureAlgorithm(int[,] graph, Func<int[,], int, (int, List<int>)> algorithm)
     {
         var times = new long[12];
+        int totalSteps = 0;
+        List<int> lastOrder = new List<int>();
+
         for (int i = 0; i < 12; i++)
         {
             var stopwatch = Stopwatch.StartNew();
-            algorithm(graph, 0);
+            var (steps, order) = algorithm(graph, 1); // Zaczynamy od węzła 1
             stopwatch.Stop();
             times[i] = stopwatch.ElapsedTicks * 100; // Przeliczanie na ns
+
+            if (i == 11) // Zapisujemy dane tylko dla ostatniej iteracji
+            {
+                totalSteps = steps;
+                lastOrder = order;
+            }
         }
-        return times;
+        return (times, totalSteps, lastOrder);
     }
 
-    // Obliczanie średniej bez min i max
     public static long CalculateAverageExcludingMinMax(long[] times)
     {
         Array.Sort(times);
         return times.Skip(1).Take(10).Sum() / 10; // Pominięcie najwolniejszego i najszybszego
     }
 
-    // Algorytm DFS
-    public static void DFS(int[,] graph, int start)
+    public static (int, List<int>) DFS(int[,] graph, int start)
     {
         bool[] visited = new bool[graph.GetLength(0)];
         Stack<int> stack = new Stack<int>();
-        stack.Push(start);
+        List<int> order = new List<int>();
+        int steps = 0;
+
+        stack.Push(start - 1); // Przesunięcie indeksów o 1
 
         while (stack.Count > 0)
         {
@@ -131,21 +146,27 @@ class Program
             if (!visited[node])
             {
                 visited[node] = true;
-                for (int i = 0; i < graph.GetLength(1); i++)
+                order.Add(node + 1); // Przesunięcie indeksów o 1
+                steps++;
+                for (int i = graph.GetLength(1) - 1; i >= 0; i--) // Odwiedzanie w odwrotnej kolejności
                 {
                     if (graph[node, i] == 1 && !visited[i])
                         stack.Push(i);
                 }
             }
         }
+
+        return (steps, order);
     }
 
-    // Algorytm BFS
-    public static void BFS(int[,] graph, int start)
+    public static (int, List<int>) BFS(int[,] graph, int start)
     {
         bool[] visited = new bool[graph.GetLength(0)];
         Queue<int> queue = new Queue<int>();
-        queue.Enqueue(start);
+        List<int> order = new List<int>();
+        int steps = 0;
+
+        queue.Enqueue(start - 1); // Przesunięcie indeksów o 1
 
         while (queue.Count > 0)
         {
@@ -153,6 +174,8 @@ class Program
             if (!visited[node])
             {
                 visited[node] = true;
+                order.Add(node + 1); // Przesunięcie indeksów o 1
+                steps++;
                 for (int i = 0; i < graph.GetLength(1); i++)
                 {
                     if (graph[node, i] == 1 && !visited[i])
@@ -160,5 +183,7 @@ class Program
                 }
             }
         }
+
+        return (steps, order);
     }
 }
